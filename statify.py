df94931304dcdcb7a925068d1c5d49c27da0a367
NBens <benshaqi.nizar@gmail.com>
2018-04-15 18:11:34 +0100

    Static Modifications

--- statify.py
+++ statify.py
@@ -7,75 +7,96 @@ from jinja2 import Template
 
 with open ("config.json", "r") as my_file:
     config = json.loads(my_file.read())
+
     
-with open("static/post.html", "r") as my_file:
-    template = Template(my_file.read())
+def loadTemplate(page):
+    templatePath = "static/{}.html".format(page)
+    with open(templatePath, "r") as my_file:
+        template = Template(my_file.read())
+    return template
 
-with open("static/page.html", "r") as my_file:
-    pageTemplate = Template(my_file.read())
+
+def extractMeta(text):
+    found = re.findall('<!--(.*?)-->', text)
+    splitted = [[j.strip(" ") for j in t.split(":")] for t in found]
+    return splitted
 
 
-i = 0
-finals = []
-for page in os.listdir("./content"):
-    i += 1
-    if (page[-3:] == ".md"):
-        content = markdown2.markdown_path("./content/"+page)
-        found = re.findall('<!--(.*?)-->', content)
-        splitted = [[j.strip(" ") for j in t.split(":")] for t in found]
-        ###############################
-        title = ""
-        subtitle = ""
-        author = config["author"]
-        footer = config["footer"]
-        pages = []
-        pageOrNot = 0
-        blogName = config["blog-name"]
+def contentFiles():
+    files = []
+    for page in os.listdir("./content"):
+        if (page[-3:] == ".md"):
+            files.append(page)
+    return files
 
-        ###############################
-        for item in splitted:
-            if item[0] == "Title":
-                title = item[1]
-                dateList = str(page)
-                dateList = dateList.split("-")
-                fileName = list(dateList[3:])
-                fileName = "-".join(fileName)
-                fileName = fileName[:-3]
-                date = dateList[0] + "-" + dateList[1] + "-" + dateList[2]
-            elif item[0] == "Author":
-                author = item[1]
-            elif item[0] == "Subtitle":
-                subtitle = item[1]
-            elif item[0] == "Page":
-                pageOrNot = 1
-                pageTitle = item[1]
-                pageHref = page[:-3] + ".html"
-                pages.append({"title": pageTitle, "href": pageHref})
-        if (pageOrNot == 0):
-            dependencies = "../../../../"
-            if not os.path.exists("output/Posts/" + dateList[0] + "/" + dateList[2] + "/" + fileName):
-                os.makedirs("output/Posts/" + dateList[0] + "/" + dateList[2] + "/" + fileName)
-            with open("output/Posts/" + dateList[0] + "/" + dateList[2] + "/" + fileName + "/index.html", "w") as my_file:
-                my_file.write(template.render(footer=footer, dependencies = dependencies, blogName = blogName, title=title, subtitle=subtitle, content=content, author=author, date=date, facebook = config["facebook"], twitter = config["twitter"], github = config["github"]))
-            finals.append("output/Posts/" + dateList[0] + "/" + dateList[2] + "/" + fileName + "/index.html")
-        else:
-            dependencies = ""
-            with open("output/" + page[:-3] + ".html", "w") as my_file:
-                my_file.write(pageTemplate.render(footer=footer, dependencies = dependencies, blogName = blogName, title=pageTitle, subtitle=subtitle, content=content, facebook = config["facebook"], twitter = config["twitter"], github = config["github"]))
-            finals.append("output/" + page[:-3] + ".html")
-    if (i == len(os.listdir("./content"))):
-        toWrite = ""
-        toWrite2 = ""
-        for pageito in pages:
-            toWrite += '\t\t\t<li class="nav-item">\n\t\t\t\t<a class="nav-link" href="{}">{}</a>\n\t\t\t</li>'.format(pageito["href"], pageito["title"])
-            toWrite2 += '\t\t\t<li class="nav-item">\n\t\t\t\t<a class="nav-link" href="../../../../{}">{}</a>\n\t\t\t</li>'.format(pageito["href"], pageito["title"])
-        for file in finals:
-            with open (file, 'r') as myFy:
-                data = myFy.read()
-            with open (file, 'w') as myFy:
-                if (len(file.split("/")) == 2):
-                    myFy.write(data.replace("[NAV-BAR]", toWrite))
-                else:
-                    myFy.write(data.replace("[NAV-BAR]", toWrite2))
-        
 
+def readFile(path):
+    with open(path, 'r') as my_file:
+        return my_file.read()
+
+
+def writeFile(path, text):
+    with open(path, 'w') as my_file:
+        return my_file.write(text)
+
+
+def dateFromFileName(file):
+    r = re.search("([0-9]{4}\-([0-9]{2}|[0-9]{1})\-([0-9]{2}|[0-9]{1}))", file)
+    return r.group(0)
+    
+
+def makedir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+files = contentFiles()
+contents = []
+
+for file in files:
+    content = markdown2.markdown_path("./content/"+file)
+    meta = extractMeta(content)
+
+    ####
+    title = ""
+    subtitle = ""
+    date = ""
+    contentType = ""
+    author = config["author"]
+    ###
+    for m in meta:
+        if (m[0] == "Page"):
+            title = m[1]
+            contentType = "page"
+            href = file[:-3] + ".html"
+        elif (m[0] == "Subtitle"):
+            subtitle = m[1]
+        elif (m[0] == "Author"):
+            author = m[1]
+        elif (m[0] == "Title"):
+            title = m[1]
+            contentType = "blog"
+            date = dateFromFileName(file)
+            href = file.replace(date,date.replace("-", "/"), 1)
+            href = href.replace("-", "/", 1)
+            href = href[:-3]
+            href = "Posts/" + href
+    contents.append({"type": contentType, "title": title, "subtitle": subtitle, "author": author, "date": date, "href": href, "content": content})
+
+
+indexTemplate = loadTemplate("index")
+writeFile("output/index.html", indexTemplate.render(blogs = contents, config=config, dependencies=""))
+
+postTemplate = loadTemplate("post")
+pageTemplate = loadTemplate("page")
+
+for c in contents:
+    if (c.get("type") == "blog"):
+        dependencies = "../../../../../"
+        filePath = "output/" + c.get("href")
+        makedir(filePath)
+        writeFile(filePath + "/index.html", postTemplate.render(blog = c, blogs=contents, config=config, dependencies=dependencies))
+    elif (c.get("type") == "page"):
+        dependencies = ""
+        filePath = "output/" + c.get("href")
+        writeFile(filePath, pageTemplate.render(content = c, blogs = contents, config=config, dependencies=dependencies))

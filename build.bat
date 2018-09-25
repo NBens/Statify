e541c58fbbee2e3bc76e112f87b290e329a58543
NBens <benshaqi.nizar@gmail.com>
2018-09-25 15:53:12 +0100

    Remove build script

--- build.bat
+++
@@ -1,9 +0,0 @@
-git add .
-git commit -m "Static Modifications"
-git push
-statify.py
-xcopy /s output ..\NBens.github.io
-cd ../NBens.github.io
-git add .
-git commit -m "New Blog Added!"
-git push
